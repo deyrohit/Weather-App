27847045d68afac8001db0e22480e23ca265ab80
deyrohit <deyrohit1212@gmail.com>
2024-03-01 10:41:44 +0530

    UI design

---
+++ app/src/main/java/gmail/deyrohit1212/weatherapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package gmail.deyrohit1212.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/gmail/deyrohit1212/weatherapp/Splashscreen.kt
@@ -0,0 +1,20 @@
+package gmail.deyrohit1212.weatherapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.os.postDelayed
+
+class Splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent =Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
